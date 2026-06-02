cfe60c4ea28918b196747202fb259ee8a6492524
Sofia <aaa@aaa.com>
2026-06-02 10:40:02 -0300

    1.2

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/estoque')
+def estoque():  # Alterado de home para estoque
+    return render_template('estoque.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
