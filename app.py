20a1a8d784a41dc87eff9c3c0a3793512bfa7787
Sofia <a@a.br>
2026-06-08 18:39:54 -0300

    1.4 (mysql + html, tabela)

--- app.py
+++ app.py
@@ -1,15 +1,56 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_mysqldb import MySQL
+import mysql.connector
 
 app = Flask(__name__)
 
+# Conexão com o banco
+def obter_conexao():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Sofia1211",
+        database="tcc_almoxarifado",
+        port=3306
+    )
+
 @app.route('/inicio')
 def home():
-    return render_template('home.html')
+    conexao = obter_conexao()
+    cursor = conexao.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM produtos")
+    produtos_do_banco = cursor.fetchall()
+
+    cursor.close()
+    conexao.close()
+
+    return render_template('home.html', produtos=produtos_do_banco)
 
 @app.route('/estoque')
-def estoque():  # Alterado de home para estoque
+def estoque():
     return render_template('estoque.html')
+#   Cadastro nao oficial
+@app.route('/cadastrar_produto', methods=['POST'])
+def cadastrar_produto():
+    nome = request.form.get('nome')
+    quantidade = request.form.get('quantidade')
+    descricao = request.form.get('descricao')
+
+    conexao = obter_conexao()
+    cursor = conexao.cursor()
+
+    comando = """
+        INSERT INTO produtos (nome, quantidade, descricao)
+        VALUES (%s, %s, %s)
+    """
+    cursor.execute(comando, (nome, quantidade, descricao))
+
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    return redirect(url_for('estoque'))
 
 if __name__ == '__main__':
     app.run(debug=True)
